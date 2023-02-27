025ceb6769daea5f6902b836c34576ae0f848a0d
wzj-tim <81309666+wzj-tim@users.noreply.github.com>
2023-02-27 15:34:58 +0800

    PureOpenLoop

--- Task2/main.py
+++ Task2/main.py
@@ -11,84 +11,38 @@ color_dist = {'red': {'Lower': np.array([0, 60, 60]), 'Upper': np.array([6, 255,
               'blue': {'Lower': np.array([80, 100, 120]), 'Upper': np.array([130, 220, 180])},
               'green': {'Lower': np.array([35, 43, 35]), 'Upper': np.array([90, 255, 255])},
               'yellow': {'Lower': np.array([25, 160, 230]), 'Upper': np.array([45, 250, 255])},
-              'yellow2': {'Lower': np.array([25, 100, 230]), 'Upper': np.array([45, 180, 255])},
-              'white': {'Lower': np.array([245, 245, 245])}, 'Upper': np.array([255, 255, 255])}
+              'yellow2': {'Lower': np.array([25, 100, 230]), 'Upper': np.array([45, 180, 255])}}
 
 
-# 使用前视角识别红块左转
-def isLeftTurning(view1):
-    # 路口识别
-    # 出口标志牌
-    # ori2=cv2.imread("./View218.jpg")
-    flag1 = flag2 = 0
-    # 可以设置上下限
-    thres1 = 420
-    thres2 = 100 * 0.56
-    ori2 = view1
-    size2 = ori2.shape
-    roi2 = ori2[int(0 / 575 * size2[0]):int(300 / 575 * size2[0]),
-           int(750 / 1023 * size2[1]):int(850 / 1023 * size2[1])]
-    blur2 = cv2.GaussianBlur(roi2, (5, 5), 0)
-    hsv_img2 = cv2.cvtColor(blur2, cv2.COLOR_BGR2HSV)
+def Stall_detection(view3):
+    # 车位识别
+    thres = 1200
+    ori1 = view3
+    size1 = ori1.shape
+    roi1 = ori1[int(150 / 575 * size1[0]):int(500 / 575 * size1[0]), int(800 / 1023 * size1[1]):]
+    blur1 = cv2.GaussianBlur(roi1, (5, 5), 0)
+    hsv_img1 = cv2.cvtColor(blur1, cv2.COLOR_BGR2HSV)
     kernel = np.ones((3, 3), dtype=np.uint8)
-    erode_hsv2 = cv2.erode(hsv_img2, kernel, iterations=1)
-    kernel = np.ones((3, 3), dtype=np.uint8)
-    dilate_hsv2 = cv2.dilate(erode_hsv2, kernel, iterations=2)
-    inRange_hsv2 = cv2.inRange(dilate_hsv2, color_dist['red']['Lower'], color_dist['red']['Upper'])
-    sum_of_red = len((inRange_hsv2[inRange_hsv2 == 255]))
-    if sum_of_red != 0:  print('sum_of_red', sum_of_red)
-    if sum_of_red >= thres1:
-        flag1 = 1
-    # cv2.imshow("roi2", roi2)
-    cv2.imshow("hsv2", inRange_hsv2)
-    cv2.waitKey(1000)
-
-    if flag1 == 1:
+    erode_hsv1 = cv2.erode(hsv_img1, kernel, iterations=1)
+    inRange_hsv1 = cv2.inRange(erode_hsv1, color_dist['yellow']['Lower'], color_dist['yellow']['Upper'])
+    sum_of_yellow = len((inRange_hsv1[inRange_hsv1 == 255]))
+    # cv2.imshow("roi1", roi1)
+    # cv2.imshow("hsv1", inRange_hsv1)
+    # cv2.waitKey(1000)
+    print('sum_of_yellow', sum_of_yellow)
+    # if sum_of_yellow > 0: print('sum_of_yellow',sum_of_yellow)
+    if sum_of_yellow > thres:
         return 1
     return 0
 
 
-def isCrossing(view1):
+def Crossing_detection(view2):
     # 路口识别
     # 出口标志牌
     # ori2=cv2.imread("./View218.jpg")
     flag1 = flag2 = 0
-    # 可以设置上下限
     thres1 = 420
     thres2 = 100 * 0.56
-    ori2 = view1
-    size2 = ori2.shape
-    roi2 = ori2[int(70 / 100 * size2[0]):int(80 / 100 * size2[0]), :]
-    blur2 = cv2.GaussianBlur(roi2, (5, 5), 0)
-    hsv_img2 = cv2.cvtColor(blur2, cv2.COLOR_BGR2HSV)
-    # kernel = np.ones((3, 3), dtype=np.uint8)
-    # erode_hsv2 = cv2.erode(hsv_img2, kernel, iterations=1)
-    # kernel = np.ones((3, 3), dtype=np.uint8)
-    # # dilate_hsv2 = cv2.dilate(erode_hsv2, kernel, iterations=2)
-    inRange_hsv2 = cv2.inRange(hsv_img2, color_dist['white']['Lower'], color_dist['white']['Upper'])
-    sum_of_red = len((inRange_hsv2[inRange_hsv2 == 255]))
-    # if sum_of_red != 0:  print('sum_of_red', sum_of_red)
-    # if sum_of_red >= thres1:
-    #     flag1 = 1
-    # cv2.imshow("roi2", roi2)
-    cv2.imshow("hsv2", inRange_hsv2)
-    cv2.waitKey(1000)
-
-    if flag1 == 1:
-        return 1
-    return 0
-
-
-# view2为左视角
-def isRightTurning(view2):
-    # 路口识别
-    # 出口标志牌
-    # ori2=cv2.imread("./View218.jpg")
-    flag1 = 0
-
-    # 可以设置上下限
-    thres = 420
-
     ori2 = view2
     size2 = ori2.shape
     roi2 = ori2[int(0 / 575 * size2[0]):int(300 / 575 * size2[0]),
@@ -99,18 +53,61 @@ def isRightTurning(view2):
     erode_hsv2 = cv2.erode(hsv_img2, kernel, iterations=1)
     kernel = np.ones((3, 3), dtype=np.uint8)
     dilate_hsv2 = cv2.dilate(erode_hsv2, kernel, iterations=2)
-    inRange_hsv2 = cv2.inRange(dilate_hsv2, color_dist['white']['Lower'], color_dist['white']['Upper'])
-    sum_of_white = len((inRange_hsv2[inRange_hsv2 == 255]))
-    if sum_of_white != 0:  print('sum_of_white', sum_of_white)
-    if sum_of_white <= thres:
+    inRange_hsv2 = cv2.inRange(dilate_hsv2, color_dist['blue']['Lower'], color_dist['blue']['Upper'])
+    sum_of_blue = len((inRange_hsv2[inRange_hsv2 == 255]))
+    if sum_of_blue != 0:  print('sum_of_blue', sum_of_blue)
+    if sum_of_blue >= thres1:
         flag1 = 1
     # cv2.imshow("roi2", roi2)
     # cv2.imshow("hsv2", inRange_hsv2)
     # cv2.waitKey(1000)
 
+    # 地面黄线
+    roi3 = ori2[int(300 / 575 * size2[0]):int(500 / 575 * size2[0]),
+           int(700 / 1023 * size2[1]):int(1000 / 1023 * size2[1])]
+    blur3 = cv2.GaussianBlur(roi3, (5, 5), 0)
+    hsv_img3 = cv2.cvtColor(blur3, cv2.COLOR_BGR2HSV)
+    kernel = np.ones((3, 3), dtype=np.uint8)
+    erode_hsv3 = cv2.erode(hsv_img3, kernel, iterations=2)
+    kernel = np.ones((3, 3), dtype=np.uint8)
+    dilate_hsv3 = cv2.dilate(erode_hsv3, kernel, iterations=1)
+    inRange_hsv3 = cv2.inRange(dilate_hsv3, color_dist['yellow2']['Lower'], color_dist['yellow2']['Upper'])
+    sum_of_white = len((inRange_hsv3[inRange_hsv3 == 255]))
+    print(sum_of_white)
+    if sum_of_white >= thres2:
+        flag2 = 1
+    # cv2.imshow("roi3", roi3)
+    # cv2.imshow("hsv3", inRange_hsv3)
+
     if flag1 == 1:
         return 1
     return 0
+
+
+def Crossing_detection2(view1):
+    # 路口识别
+    # 出口标志牌
+    ori4 = view1
+    thres = 340
+    size4 = ori4.shape
+    # print(size2)
+    roi4 = ori4[int(0 / 575 * size4[0]):int(250 / 575 * size4[0]),
+           int(200 / 1023 * size4[1]):int(500 / 1023 * size4[1])]
+    blur4 = cv2.GaussianBlur(roi4, (5, 5), 0)
+    hsv_img4 = cv2.cvtColor(blur4, cv2.COLOR_BGR2HSV)
+    inRange_hsv4 = cv2.inRange(hsv_img4, color_dist['blue']['Lower'], color_dist['blue']['Upper'])
+    kernel = np.ones((3, 3), dtype=np.uint8)
+    erode_hsv4 = cv2.erode(inRange_hsv4, kernel, iterations=1)
+    kernel = np.ones((3, 3), dtype=np.uint8)
+    inRange_hsv4 = cv2.dilate(erode_hsv4, kernel, iterations=3)
+    sum_of_white = len((inRange_hsv4[inRange_hsv4 == 255]))
+    print('--', sum_of_white, '--')
+    if sum_of_white >= thres:
+        return 1
+    return 0
+    # cv2.imshow("ori4", ori4)
+    # cv2.imshow("roi4", roi4)
+    # cv2.imshow("hsv4", inRange_hsv4)
 
 
 class counter:
@@ -144,10 +141,9 @@ class counter:
 
 
 def image_to_speed(view1, view2, view3, view4, state):
-    viewFront = cv2.imdecode(view1, cv2.IMREAD_ANYCOLOR)
-    viewBack = cv2.imdecode(view1, cv2.IMREAD_ANYCOLOR)
-    viewLeft = cv2.imdecode(view2, cv2.IMREAD_ANYCOLOR)
-    viewRight = cv2.imdecode(view3, cv2.IMREAD_ANYCOLOR)
+    view1 = cv2.imdecode(view1, cv2.IMREAD_ANYCOLOR)
+    view2 = cv2.imdecode(view2, cv2.IMREAD_ANYCOLOR)
+    view3 = cv2.imdecode(view3, cv2.IMREAD_ANYCOLOR)
     left_speed = 0
     right_speed = 0
 
@@ -158,64 +154,71 @@ def image_to_speed(view1, view2, view3, view4, state):
     # state machine
     if curState == 0:
         # 初始化状态1所需的计数器
+        counter1.setCounter(18)
         left_speed = 0
         right_speed = 0
 
         # 状态转移
-        state.set(1)
+        state.set(curState + 1)
 
-    # 高速直行
+    # 直行
     elif curState == 1:
-        left_speed = 1
-        right_speed = 1
+        counter1.updateCounter()
+        left_speed = 2
+        right_speed = 2
 
         # 状态转移
-        if isCrossing(viewFront):
-            state.set(2)
-
-    # 低速直行
-    elif curState == 2:
-        left_speed = 0.5
-        right_speed = 0.5
-
-        # 状态转移
-        if isRightTurning(viewLeft):
-            counter1.setCounter(10)
-            state.set(4)
-
-        # 状态转移
-        if isLeftTurning(viewFront):
-            counter1.setCounter(10)
-            state.set(5)
-
-    # 停车
-    elif curState == 3:
-        left_speed = 0
-        right_speed = 0
-
-        # # 状态转移
-        # if isRightTurning(view1):
-        #     state.set(3)
+        if counter1.isZero():
+            state.set(curState + 1)
+            counter1.setCounter(9)
 
     # 右转
+    elif curState == 2:
+        counter1.updateCounter()
+        left_speed = 1.789
+        right_speed = 1.5
+
+        if counter1.isZero():
+            state.set(curState + 1)
+            counter1.setCounter(120)
+
+    elif curState == 3:
+        counter1.updateCounter()
+        left_speed = 3.0
+        right_speed = 3.0
+
+        if counter1.isZero():
+            state.set(curState + 1)
+            counter1.setCounter(9)
+
     elif curState == 4:
         counter1.updateCounter()
-        left_speed = 1.1
-        right_speed = 1
+        left_speed = 1.5
+        right_speed = 1.789
 
         if counter1.isZero():
-            state.set(1)
+            state.set(curState + 1)
+            counter1.setCounter(20)
 
-    # 左转
     elif curState == 5:
         counter1.updateCounter()
-        left_speed = 1
-        right_speed = 1.1
+        left_speed = 3.0
+        right_speed = 3.0
 
         if counter1.isZero():
-            state.set(1)
+            state.set(curState + 1)
+            counter1.setCounter(10)
+
+    elif curState == 6:
+        # counter1.updateCounter()
+        left_speed = 0
+        right_speed = 0
+
+        if counter1.isZero():
+            state.set(curState + 1)
+            counter1.setCounter(10)
 
     print('cur state is', curState)
-    # print('cur counter is', counter1.getVal())
+    print('cur counter is', counter1.getVal())
 
-    return left_speed, right_speed, 0, 0
+    return left_speed, right_speed, 1, 1
