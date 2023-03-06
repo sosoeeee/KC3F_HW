b676e89f78b53bad56f3b0a630041e9df8999043
sosoeeee <809953371@qq.com>
2023-03-06 14:44:45 +0800

    sum White

--- Task2/main.py
+++ Task2/main.py
@@ -12,7 +12,7 @@ color_dist = {'red': {'Lower': np.array([0, 60, 60]), 'Upper': np.array([6, 255,
               'green': {'Lower': np.array([35, 43, 35]), 'Upper': np.array([90, 255, 255])},
               'yellow': {'Lower': np.array([25, 160, 230]), 'Upper': np.array([45, 250, 255])},
               'yellow2': {'Lower': np.array([25, 100, 230]), 'Upper': np.array([45, 180, 255])},
-              'white': {'Lower': np.array([0, 0, 221]), 'Upper': np.array([180, 30, 255])}}
+              'white': {'Lower': np.array([0, 0, 200]), 'Upper': np.array([180, 30, 255])}}
 
 
 # 使用前视角识别红块左转
@@ -34,7 +34,7 @@ def isLeftTurning(view1):
     erode_hsv2 = cv2.erode(hsv_img2, kernel, iterations=1)
     kernel = np.ones((3, 3), dtype=np.uint8)
     dilate_hsv2 = cv2.dilate(erode_hsv2, kernel, iterations=2)
-    inRange_hsv2 = cv2.inRange(dilate_hsv2, color_dist['red']['Lower'], color_dist['red']['Upper'])
+    inRange_hsv2 = cv2.inRange(hsv_img2, color_dist['red']['Lower'], color_dist['red']['Upper'])
     sum_of_red = len((inRange_hsv2[inRange_hsv2 == 255]))
     if sum_of_red != 0:  print('sum_of_red', sum_of_red)
     if sum_of_red >= thres1:
@@ -53,22 +53,25 @@ def isCrossing(view1):
     # 出口标志牌
     flag1 = 0
     # 可以设置上下限
-    thres1 = 420
-    thres2 = 100 * 0.56
+    thres1 = 10000
     ori2 = view1
     size2 = ori2.shape
     roi2 = ori2[int(70 / 100 * size2[0]):int(80 / 100 * size2[0]), :]
     blur2 = cv2.GaussianBlur(roi2, (5, 5), 0)
     hsv_img2 = cv2.cvtColor(blur2, cv2.COLOR_BGR2HSV)
+    inRange_hsv2 = cv2.inRange(hsv_img2, color_dist['white']['Lower'], color_dist['white']['Upper'])
+
     kernel = np.ones((3, 3), dtype=np.uint8)
-    dilate_hsv2 = cv2.dilate(hsv_img2, kernel, iterations=2)
+    dilate_hsv2 = cv2.dilate(inRange_hsv2, kernel, iterations=4)
     kernel = np.ones((3, 3), dtype=np.uint8)
     erode_hsv2 = cv2.erode(dilate_hsv2, kernel, iterations=1)
-    inRange_hsv2 = cv2.inRange(erode_hsv2, color_dist['white']['Lower'], color_dist['white']['Upper'])
-    sum_of_white = len((inRange_hsv2[inRange_hsv2 == 255]))
-    if sum_of_white != 0:  print('sum_of_red', sum_of_white)
-    # if sum_of_red >= thres1:
-    #     flag1 = 1
+
+    edgePicture =
+
+    sum_of_white = len((erode_hsv2[erode_hsv2 == 255]))
+    if sum_of_white != 0:  print('sum_of_white', sum_of_white)
+    if sum_of_white >= thres1:
+        flag1 = 1
     # cv2.imshow("roi2", roi2)
     cv2.imshow("hsv2", inRange_hsv2)
     cv2.waitKey(1000)
