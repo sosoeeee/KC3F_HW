f1f2868db396143aa12c8222a5ae6617f621e749
sosoeeee <809953371@qq.com>
2023-04-03 17:33:24 +0800

    add filter

--- entityCar/Code/main.py
+++ entityCar/Code/main.py
@@ -6,53 +6,6 @@ import undistortionLib
 import time
 
 
-def findLine(img, grayFlag=False):
-    if grayFlag:
-        # 大津法二值化
-        ret, binary = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    else:
-        binary = img
-    # canny边缘检测
-    canny = cv2.Canny(binary, 50, 150)
-
-    direction = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
-    length, width = binary.shape
-    trace = [[]]
-    trace_k = 0
-
-    for i in range(length - 1, -1, -1):
-        for j in range(width):
-            if canny[i, j] == 255:
-                trace[trace_k].append([j, i])
-                canny[i, j] = 0
-                k = 0
-                while 1:
-                    for cnt in range(8):
-                        k = k % 8
-                        x = i + direction[k][0]
-                        y = j + direction[k][1]
-                        if length > x >= 0 and width > y >= 0:
-                            if canny[x, y] == 255:
-                                trace[trace_k].append([y, x])
-                                canny[x, y] = 0
-                                i = x
-                                j = y
-                                k = k - 2
-                                break
-                        k = k + 1
-
-                    if cnt == 7:
-                        if len(trace[trace_k]) < 10:
-                            trace.pop()
-                            trace.append([])
-                        else:
-                            trace_k = trace_k + 1
-                            trace.append([])
-                        break
-
-    return trace
-
-
 class Car:
     def __init__(self):
         self.car = driver()
@@ -84,14 +37,70 @@ class PID:
         self.integral = 0
 
 
+class Path:
+    def __init__(self):
+        self.filter = False
+        self.path = [[], [], []]
+        self.filtedPath = [[], [], []]
+        # 低通IIR滤波器参数
+        self.b = [0.5, 0.3, 0.2]
+        self.a = [0.5, 0.3, 0.2]
+
+    def recPath(self, edgeImg, filter=False):
+        self.filter = filter
+        self.path[2] = self.path[1]
+        self.path[1] = self.path[0]
+        self.path[0] = []
+
+        edgeImg_cut = edgeImg[140:480, 220:420]
+        length, width = edgeImg_cut.shape
+        # 提取轨迹点
+        for y in range(0, length, 10):
+            pointXSet = np.transpose(np.nonzero(edgeImg_cut[y, :]))
+            if len(pointXSet) > 0:
+                interestPoint = np.sum(pointXSet, axis=0) / len(pointXSet)
+                self.path[0].append([int(interestPoint), y])
+
+        if self.filter:
+            self.filtedPath[2] = self.filtedPath[1]
+            self.filtedPath[1] = self.filtedPath[0]
+            self.filtedPath[0] = []
+
+            if len(self.path[1]) > 0 and len(self.path[1]) > 0:
+                self.filtedPath[0] = (self.b[0] * np.array(self.path[0])
+                                      + self.b[1] * np.array(self.path[1])
+                                      + self.b[2] * np.array(self.path[2])
+                                      - self.a[1] * np.array(self.filtedPath[1])
+                                      - self.a[2] * np.array(self.filtedPath[2])) / self.a[0]
+                self.filtedPath[0] = self.filtedPath[0].tolist()
+            else:
+                self.filtedPath[0] = self.path[0]
+
+    def filterPath(self):
+        return self.filtedPath[0]
+
+    def rawPath(self):
+        return self.path[0]
+
+    def drawPath(self, img):
+        if self.filter:
+            for point in self.filtedPath[0]:
+                cv2.circle(img, (point[0], point[1]), 1, (0, 255, 0), 2)
+        for point in self.path[0]:
+            cv2.circle(img, (point[0], point[1]), 1, (0, 0, 255), 2)
+        return img
+
+
 def main():
     car = Car()
     cap = cv2.VideoCapture(0)
     lastTime = time.time()
+    pathObserver = Path()
     while True:
         _, frame = cap.read()
         # 转换为灰度图
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # 相机畸变矫正
         caliber_gray = undistortionLib.grayUndistort(gray)
         # 大津法二值化
         ret, binary = cv2.threshold(caliber_gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
@@ -102,26 +111,14 @@ def main():
         canny = cv2.Canny(binary, 50, 150)
         # 提取图像局部
         canny_cut = canny[140:480, 220:420]
-        color_gray = cv2.cvtColor(canny_cut, cv2.COLOR_GRAY2BGR)
         length, width = canny_cut.shape
-        interestPointSet = []
-        for y in range(0, length, 10):
-            pointXSet = np.transpose(np.nonzero(canny_cut[y, :]))
-            if len(pointXSet) > 0:
-                interestPoint = np.sum(pointXSet, axis=0) / len(pointXSet)
-                interestPointSet.append((int(interestPoint), y))
-        # print(interestPointSet)
-
-        # # Hough直线检测
-        # lines = cv2.HoughLinesP(canny, 1, np.pi / 180, 100, minLineLength=40, maxLineGap=20)
-        # # 绘制直线
-        # if lines is not None:
-        #     for line in lines:
-        #         x1, y1, x2, y2 = line[0]
-        #         cv2.line(color_gray, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
-        for point in interestPointSet:
-            cv2.circle(color_gray, point, 1, (0, 0, 255), 2)
+        # 轨迹提取
+        pathObserver.recPath(canny_cut, filter=True)
+        # path = pathObserver.filterPath()
+        # path = pathObserver.rawPath()
+        # 轨迹绘制
+        color_gray = cv2.cvtColor(canny_cut, cv2.COLOR_GRAY2BGR)
+        color_gray = pathObserver.drawPath(color_gray)
 
         cv2.imshow("image", color_gray)
         # cv2.imshow("image1", canny)
