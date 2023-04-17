6dcef2e85149027d7d0b9bdc3b6f81e9e15e253f
sosoeeee <809953371@qq.com>
2023-04-17 16:29:45 +0800

    add Harris point detection

--- entityCar/Code/main.py
+++ entityCar/Code/main.py
@@ -17,24 +17,37 @@ class Car:
         return self.car.read_battery()
 
 
+car = Car()
+
+
 # PID class
 class PID:
     def __init__(self, kp, ki, kd):
         self.kp = kp
         self.ki = ki
         self.kd = kd
-        self.last_error = 0
-        self.integral = 0
+        self.lastError = 0
+        self.sumError = 0
 
-    def update(self, error, dt):
-        self.integral += error * dt
-        derivative = (error - self.last_error) / dt
-        self.last_error = error
-        return self.kp * error + self.ki * self.integral + self.kd * derivative
+    def update(self, error):
+        self.sumError += error
+        deltaError = error - self.lastError
+        self.lastError = error
+        return self.kp * error + self.ki * self.sumError + self.kd * deltaError
 
-    def reset(self):
-        self.last_error = 0
-        self.integral = 0
+
+def GetWheelSpeed(dots, centre, para, controller):
+    dots[:, 0] = dots[:, 0] - centre
+    # print(para)
+    # controlVariable = np.dot(dots[:, 0], para) * 0.15  # 误差为正需要右转，左轮加速
+    error = np.dot(dots[:, 0], para)
+    controlVariable = controller.update(error)
+    # print(controlVariable)
+    lSpeed = 25 - controlVariable
+    rSpeed = 25 + controlVariable
+
+    # return [lSpeed, rSpeed]
+    return [0, 0]
 
 
 class Path:
@@ -43,8 +56,8 @@ class Path:
         self.path = [[], [], []]
         self.filtedPath = [[], [], []]
         # 低通IIR滤波器参数
-        self.b = [0.5, 0.3, 0.2]
-        self.a = [0.5, 0.3, 0.2]
+        self.b = [0.3913, 0.7827, 0.3913]
+        self.a = [1, 0.3695, 0.1958]
 
     def recPath(self, edgeImg, filter=False):
         self.filter = filter
@@ -52,11 +65,9 @@ class Path:
         self.path[1] = self.path[0]
         self.path[0] = []
 
-        edgeImg_cut = edgeImg[140:480, 220:420]
-        length, width = edgeImg_cut.shape
-        # 提取轨迹点
+        length, width = edgeImg.shape
         for y in range(0, length, 10):
-            pointXSet = np.transpose(np.nonzero(edgeImg_cut[y, :]))
+            pointXSet = np.transpose(np.nonzero(edgeImg[y, :]))
             if len(pointXSet) > 0:
                 interestPoint = np.sum(pointXSet, axis=0) / len(pointXSet)
                 self.path[0].append([int(interestPoint), y])
@@ -66,7 +77,7 @@ class Path:
             self.filtedPath[1] = self.filtedPath[0]
             self.filtedPath[0] = []
 
-            if len(self.path[1]) > 0 and len(self.path[1]) > 0:
+            if len(self.path[1]) == len(self.path[0]) and len(self.path[2]) == len(self.path[0]):
                 self.filtedPath[0] = (self.b[0] * np.array(self.path[0])
                                       + self.b[1] * np.array(self.path[1])
                                       + self.b[2] * np.array(self.path[2])
@@ -85,17 +96,28 @@ class Path:
     def drawPath(self, img):
         if self.filter:
             for point in self.filtedPath[0]:
-                cv2.circle(img, (point[0], point[1]), 1, (0, 255, 0), 2)
+                cv2.circle(img, (int(point[0]), int(point[1])), 1, (0, 255, 0), 2)
         for point in self.path[0]:
             cv2.circle(img, (point[0], point[1]), 1, (0, 0, 255), 2)
         return img
 
 
+def timerTurn(speed, direct, t):
+    startTime = time.time()
+    if direct:
+        offset = 5
+    else:
+        offset = -5
+    while (time.time() - startTime) < t:
+        car.setSpeed(speed + offset, speed - offset)
+
+
 def main():
-    car = Car()
+    global car
     cap = cv2.VideoCapture(0)
     lastTime = time.time()
     pathObserver = Path()
+    controller = PID(0.15, 0, 10)
     while True:
         _, frame = cap.read()
         # 转换为灰度图
@@ -107,28 +129,60 @@ def main():
         # 二值化闭运算
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
         binary = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel)
-        # canny边缘检测
-        canny = cv2.Canny(binary, 50, 150)
-        # 提取图像局部
-        canny_cut = canny[140:480, 220:420]
-        length, width = canny_cut.shape
-        # 轨迹提取
-        pathObserver.recPath(canny_cut, filter=True)
-        # path = pathObserver.filterPath()
+        binary_ROI = binary[140:480, 220:420]
+
+        # Harris角点检测
+        dst = cv2.cornerHarris(binary_ROI, 10, 3, 0.18)
+
+        color_gray = cv2.cvtColor(binary_ROI, cv2.COLOR_GRAY2BGR)
+        color_gray[dst > 0.4 * dst.max()] = [0, 0, 255]
+        boolMar = dst > 0.4 * dst.max()
+        loc = np.transpose(np.nonzero(boolMar))
+        loc_filter = []
+        if len(loc) > 0:
+            loc_filter = [loc[0]]
+            for i in range(len(loc) - 1):
+                for point in loc_filter:
+                    if abs(loc[i + 1][0] - point[0]) < 10 or abs(loc[i + 1][1] - point[1]) < 10:
+                        break
+                loc_filter.append(loc[i+1])
+        print("raw", len(loc))
+        print("filter", len(loc_filter))
+        # print(loc)
+        # print('--------------------')
+
+        # # canny边缘检测
+        # canny = cv2.Canny(binary_ROI, 50, 150)
+        # # 提取图像局部
+        # length, width = canny.shape
+        # # 轨迹提取
+        # pathObserver.recPath(canny_cut, filter=True)
+        # # path = pathObserver.filterPath()
         # path = pathObserver.rawPath()
-        # 轨迹绘制
-        color_gray = cv2.cvtColor(canny_cut, cv2.COLOR_GRAY2BGR)
-        color_gray = pathObserver.drawPath(color_gray)
+        # # 轨迹绘制
+        # color_gray = cv2.cvtColor(canny_cut, cv2.COLOR_GRAY2BGR)
+        # color_gray = pathObserver.drawPath(color_gray)
+        #
+        # # 轨迹控制
+        # if len(path) > 0:
+        #     # 计算轨迹中心
+        #     path = np.array(path)
+        #     centre = int(width/2)
+        #     # 计算轨迹方向
+        #     weight = float(1.0 / len(path))
+        #     para = np.ones(len(path)) * weight
+        #     # 计算轮速
+        #     wheelSpeed = GetWheelSpeed(path, centre, para, controller)
+        #     # 控制小车
+        #     car.setSpeed(wheelSpeed[0], wheelSpeed[1])
 
         cv2.imshow("image", color_gray)
-        # cv2.imshow("image1", canny)
-        cv2.waitKey(3)
+        # # cv2.imshow("image1", canny)
+        cv2.waitKey(1)
 
-        tmpTime = time.time()
-        print(1 / (tmpTime - lastTime))
-        lastTime = tmpTime
-
-        car.setSpeed(0, 0)
+        # tmpTime = time.time()
+        # print(1 / (tmpTime - lastTime))
+        # lastTime = tmpTime
 
 
 if __name__ == "__main__":
